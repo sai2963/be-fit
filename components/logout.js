954bd78fd52d8ebe47b06cf1571160bc2b59d389
Sai2963 <venkatsai0629@gmail.com>
2024-11-01 14:55:19 +0530

    commit infinite

--- components/logout.js
+++ components/logout.js
@@ -8,6 +8,6 @@ export const SignOutButton = () => {
     return (
         // Clicking this button signs out a user
         // and redirects them to the home page "/".
-        <button onClick={() => signOut({ redirectUrl: '/sign-in' })} className="relative inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg shadow-lg shadow-purple-500/25 hover:from-purple-500 hover:to-pink-500 transform hover:scale-105 transition-all duration-200">Sign out</button>
+        <button onClick={() => signOut({ redirectUrl: '/sign-in/'+slug })} className="relative inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg shadow-lg shadow-purple-500/25 hover:from-purple-500 hover:to-pink-500 transform hover:scale-105 transition-all duration-200">Sign out</button>
     )
 }
