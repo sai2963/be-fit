f322cbbdfe896384bd5691d3e1bf9d3d05917d1c
Sai2963 <venkatsai0629@gmail.com>
2024-12-01 12:47:31 +0530

    Sligt changes done in the project

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -10,7 +10,7 @@ import { getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAw2iX5G3pYpnEnQvQn0BOanpRsf0n0YDY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "be-fit-49443.firebaseapp.com",
   projectId: "be-fit-49443",
   storageBucket: "be-fit-49443.appspot.com",
