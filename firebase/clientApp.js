d17f9af25b6e45ed3fb5ca56723f8fe7a54c220a
Sai2963 <venkatsai0629@gmail.com>
2024-12-01 12:50:57 +0530

    Sligt changes done in the project

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -4,11 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "be-fit-49443.firebaseapp.com",
